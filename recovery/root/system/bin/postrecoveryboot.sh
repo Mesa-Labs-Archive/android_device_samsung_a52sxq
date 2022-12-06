5f47c60f20b5eaad018aea24397cf32c943370b6
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-12-06 09:14:10 +0100

    a52sxq: disable flashing of stock recovery
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>
    Change-Id: I6263e741d23a2357280702f540a11afe83cb5db3

---
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -0,0 +1,31 @@
+#!/sbin/sh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+mkdir -p "/tmp/vendor";
+blockdev --setrw "/dev/block/mapper/vendor";
+mount -w "/dev/block/mapper/vendor" "/tmp/vendor";
+
+if [ -f "/tmp/vendor/recovery-from-boot.p" ]; then
+  echo "I:postrecoveryboot: Removing stock recovery file in /vendor to prevent the stock ROM from replacing TWRP." >> /tmp/recovery.log;
+  rm "/tmp/vendor/bin/install-recovery.sh";
+  rm "/tmp/vendor/etc/init/vendor_flash_recovery.rc";
+  rm "/tmp/vendor/recovery-from-boot.p";
+fi;
+
+umount "/tmp/vendor";
+rm -r "/tmp/vendor";
+blockdev --setro "/dev/block/mapper/vendor";
+
+exit 0;
