48a6d86462a7276e87238f2d55a179b7fae11509
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-12-09 14:51:29 +0100

    a52sxq: move kernel modules in /lib/modules
    Change-Id: I904b2bff97b688dfb9d22a9a31085f0800ec510b
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- device.mk
+++ device.mk
@@ -37,13 +37,3 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
 # Screen
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2400
-
-# Copy modules for depmod
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/sec_cmd.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/sec_cmd.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/sec_common_fn.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/sec_common_fn.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/sec_secure_touch.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/sec_secure_touch.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/sec_tclm_v2.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/sec_tclm_v2.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/sec_tsp_dumpkey.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/sec_tsp_dumpkey.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/sec_tsp_log.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/sec_tsp_log.ko \
-    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/stm_ts.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/stm_ts.ko
